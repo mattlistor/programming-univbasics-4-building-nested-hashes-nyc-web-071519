9a7d4ded5db1062b51fe214e145e2c1486fc82e0
Matthew Listor <mattlistor@gmail.com>
2019-07-11 03:05:27 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -1,5 +1,6 @@
 def monopoly
 	monopoly = {:railroads => {}}
+	
 end
 
 def monopoly_with_second_tier
@@ -31,7 +32,7 @@ def monopoly_with_third_tier
       }
 	  }
 	}
-
+	
 end
 
 def monopoly_with_fourth_tier
